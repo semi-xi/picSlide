a102bc25332fb774cca373a743cf2c389878f80f
semi-xi <yezi_jun@sina.cn>
2015-07-06 16:36:42 +0800

    未解
    现在卡在size改变之后的还原 特别是当小屏滚到最后一个 然后 变成大屏的情况

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -17,16 +17,24 @@ define('carousel',function(require, exports, module){
 			autoplay:false,
 			duration: 300,
 			moveDis:0,
-			steps:2
+			steps:2,
+			autoplay:false,
+			autoplayTime:2000,
+			showWH:0,
+			_timer:null,
+			_screenShowLen:0
 		}
 		this.config=this._extend(ops);
 		this._init();
 	}
 	Carousel.prototype._init=function(){
 		this.config.length=$(this.config.element).find(this.config.content).children().length;
+		this.config.activeIndex=0;
 		this._setMoveDis();
 		this._initContent();
 		this._bindTrigger();
+		this._setAutoPlay();
+		this._resize();
 	}
 	Carousel.prototype._initContent=function(){
 		var direction=this._moveDirection();
@@ -57,33 +65,37 @@ define('carousel',function(require, exports, module){
 	}
 	Carousel.prototype._setMoveDis=function(){
 		//找到最多能走几次  以及 最后一步走多少
+		var _this=this;
+		// var showWH=this.config.showWH
 		this.config.moveDis=this._getMoveDis(this.config.direction); //确定每一步走多少，以及方向 li的高度或者宽度
 		var direction=this._moveDirection();
 		var showLen=1 / this.get('steps','attr');//一屏显示多少个内容
 		var disLen=0;
-		var showWH=0;
+
 		switch(direction){
 			case 'left' :
-				showWH=$(this.config.element).width(); //显示的高度
-
-				showLen=showWH / this.config.moveDis;
+				this.config.showWH=$(this.config.element).width(); //显示的高度
+				showLen=this.config.showWH / this.config.moveDis;
 				disLen=parseInt(showLen) - showLen //判断是不是整数 向上取整
-				this.config.maxMoveSteps=Math.ceil((this.get('length','attr')  - ($(this.config.element).width() / this.config.moveDis))) / this.get('steps','attr'); //最多能走多少步  总个数 - 显示的多少个
+
+				this.config.maxMoveSteps=Math.ceil(Math.ceil((this.get('length','attr')  - ($(this.config.element).width() / this.config.moveDis))) / this.get('steps','attr')); //最多能走多少步  总个数 - 显示的多少个
 				break;
 			case 'top' :
-				showWH=$(this.config.element).height(); //显示的高度
-				showLen=showWH / this.config.moveDis;
+				this.config.showWH=$(this.config.element).height(); //显示的高度
+				showLen=this.config.showWH / this.config.moveDis;
 				disLen=parseInt(showLen) - showLen //判断是不是整数 向上取整
-				this.config.maxMoveSteps=Math.ceil((this.get('length','attr') - ($(this.config.element).height() / this.config.moveDis))) / this.get('steps','attr'); //最多能走多少步
+				this.config.maxMoveSteps=Math.ceil(Math.ceil((this.get('length','attr') - ($(this.config.element).height() / this.config.moveDis))) / this.get('steps','attr')); //最多能走多少步
 				break;
 		}
+
+		this.config._screenShowLen= Math.ceil(this.config.showWH / (this.config.moveDis * this.get('steps','attr')));
 
 		switch(disLen == 0){ //判断是不是整数
 			case true : //整数的话最后一步不需要走
 				this.config.excessDis=0;
 				break;
 			case false :  //非整数的话应该需要走多一步完成剩下的距离
-				this.config.excessDis = this.get('maxMoveSteps','attr') * this.get('moveDis','attr') * this.get('steps','attr') - showWH;
+				this.config.excessDis =  this.get('_screenShowLen','attr') * this.get('moveDis','attr') * this.get('steps','attr') - this.config.showWH;
 				break;
 		}
 
@@ -114,6 +126,7 @@ define('carousel',function(require, exports, module){
 		}
 	}
 	Carousel.prototype.switchTo=function(fromIndex,toIndex){
+		console.log(fromIndex,toIndex)
 		if(fromIndex==toIndex) return;
 		var child=this.get(this.config.content,'ele').children(0);
 		this._upDateDisBtn();
@@ -121,41 +134,54 @@ define('carousel',function(require, exports, module){
 		var dis=this.get('moveDis','attr');
 
 		var val=0;
+		var moveVal={};
 		switch(this._moveDirection()){
 			case 'top':
 				if(toIndex == (this.config.maxMoveSteps ) || toIndex == 0){
-					console.log('最后一次了')
+					// console.log('最后一次了')
 					//距离 * 移动的距离 + 最后一次需要移动的距离 * 方向
 					//方向是用自己/绝对值自己
 					// val=(dis * ( fromIndex -  toIndex) * this.get('steps','attr') + this.config.excessDis * (( fromIndex -  toIndex)/Math.abs(( fromIndex -  toIndex))));
+
+
 					val=this.config.excessDis * (( fromIndex -  toIndex)/Math.abs(( fromIndex -  toIndex)))
 					if(val < 0) val= val + parseInt(child.css('marginBottom')); //最后一次的时候，当如果小于0的时候 往左走 应该把右边距忽略掉，这样可以让右边永远跟盒子的右边贴边 本身每次走的时候都会加上自己的边距
+					if(val >= 0) val= val - parseInt(child.css('marginBottom'))
 
 				} else{
 					val=dis * ( fromIndex -  toIndex) *  this.get('steps','attr')
 				};
+				moveVal.top=this._transformVal()[1] - 0 + val;
 				if(this._isSupportCss3()){
-					this._css3Move('top',val)
+
+					this._css3Move('top',moveVal)
 				} else{
-					this._css2Move('top',val)
+					this._css2Move('top',moveVal)
 				}
 				break;
 			case 'left':
 				if(toIndex == (this.config.maxMoveSteps ) || toIndex == 0){
-					console.log('最后一次了')
+					// console.log('最后一次了')
 					//距离 * 移动的距离 + 最后一次需要移动的距离 * 方向
 					//方向是用自己/绝对值自己
 					// val=(dis * ( fromIndex -  toIndex) * this.get('steps','attr') + this.config.excessDis * (( fromIndex -  toIndex)/Math.abs(( fromIndex -  toIndex))));
 					val=this.config.excessDis * (( fromIndex -  toIndex)/Math.abs(( fromIndex -  toIndex)));
-					if(val < 0) val= val + parseInt(child.css('marginRight')); //最后一次的时候，当如果小于0的时候 往左走 应该把右边距忽略掉，这样可以让右边永远跟盒子的右边贴边 本身每次走的时候都会加上自己的边距
 
+					if(val < 0) val= val + parseInt(child.css('marginRight')); //最后一次的时候，当如果小于0的时候 往左走 应该把右边距忽略掉，这样可以让右边永远跟盒子的右边贴边 本身每次走的时候都会加上自己的边距
+					if(val >= 0) val= val - parseInt(child.css('marginLeft'));
+					// console.log(333)
+					moveVal.left=this._transformVal()[0] - 0 + val;
+					console.log('结束')
 				} else{
-					val=dis * ( fromIndex -  toIndex) * this.get('steps','attr')
+					val=dis * ( fromIndex -  toIndex) * this.get('steps','attr');
+					moveVal.left=this._transformVal()[0] - 0 + val;
 				};
+
+				console.log(this._transformVal()[0] - 0 , val , moveVal.left)
 				if(this._isSupportCss3()){
-					this._css3Move('left',val)
+					this._css3Move('left',moveVal)
 				} else{
-					this._css2Move('left',val)
+					this._css2Move('left',moveVal)
 				}
 				break;
 		}
@@ -229,24 +255,21 @@ define('carousel',function(require, exports, module){
 	Carousel.prototype._css3Move=function(direction,val){
 		var contentBox=this.get(this.config.content,'ele');
 		var dur=this.get('duration','attr');
-		var transformVal=0;
 		switch(direction){
 			case 'top' :
-			transformVal=this._transformVal()[1] - 0+val;
 			contentBox.css({
-				'transform: ' : 'translate(0,'+ transformVal  +'px)',
-				'webkitTransform':'translate(0,'+ transformVal  +'px)',
-				'MozTransform':'translate(0,'+  transformVal  +'px)',
-				'msTransform':'translate(0,'+  transformVal  +'px)'
+				'transform: ' : 'translate(0,'+ val.top  +'px)',
+				'webkitTransform':'translate(0,'+ val.top  +'px)',
+				'MozTransform':'translate(0,'+  val.top  +'px)',
+				'msTransform':'translate(0,'+  val.top  +'px)'
 			});
 			break;
 			case 'left' :
-			transformVal=this._transformVal()[0] - 0+val;
 			contentBox.css({
-				'transform: ' : 'translate('+ transformVal  +'px, 0 )',
-				'webkitTransform':'translate('+ transformVal  +'px, 0)',
-				'MozTransform':'translate('+  transformVal  +'px, 0)',
-				'msTransform':'translate('+  transformVal  +'px, 0)'
+				'transform: ' : 'translate('+ val.left  +'px, 0 )',
+				'webkitTransform':'translate('+ val.left  +'px, 0)',
+				'MozTransform':'translate('+  val.left  +'px, 0)',
+				'msTransform':'translate('+  val.left  +'px, 0)'
 			});
 			// contentBox.animate({
 			// 	'transform: ' : 'translate(0,'+ transformVal  +'px)',
@@ -262,12 +285,12 @@ define('carousel',function(require, exports, module){
 		switch(direction){
 			case 'top' :
 			contentBox.animate({
-				'top' : '+=' + val + 'px'
+				'top' :  val + 'px'
 			},dur);
 			break;
 			case 'left' :
 			contentBox.animate({
-				'left' : '+=' + val + 'px'
+				'left' :  val + 'px'
 			},dur);
 			break;
 		}
@@ -289,6 +312,77 @@ define('carousel',function(require, exports, module){
 			case 'OTransform' in document.documentElement.style: return contentBox[0].style.webkitTransform.match(reg);break;
 			case 'msTransform' in document.documentElement.style: return contentBox[0].style.webkitTransform.match(reg);break;
 		}
+	}
+	Carousel.prototype._setAutoPlay=function(){
+		var _this=this;
+		if(this.get('autoplay','attr')){
+			this.config._addSub=1;
+			this.config._timer=setInterval(autoMoveFn,this.get('autoplayTime','attr'))
+			$(this.config.element).on('mouseover',function  () {
+				clearInterval(_this.config._timer)
+			}).on('mouseleave',function(){
+				_this.config._timer=setInterval(autoMoveFn,_this.get('autoplayTime','attr'))
+			})
+		}
+		function autoMoveFn(){
+			_this.config.prevIndex=_this.get('activeIndex','attr');
+			switch (_this.config._addSub){
+				case 1:
+					_this.config.activeIndex++;
+					if(_this.config.activeIndex > _this.config.maxMoveSteps){
+						_this.config.activeIndex=_this.config.maxMoveSteps;
+						_this.config._addSub=-1;
+					}
+					break;
+				case -1:
+					_this.config.activeIndex--;
+					if(_this.config.activeIndex < 0){
+						_this.config.activeIndex=0;
+						_this.config._addSub=1;
+					}
+					break;
+			}
+			// console.log(_this.config.prevIndex,_this.config.activeIndex)
+			_this.switchTo(_this.config.prevIndex,_this.config.activeIndex)
+		}
+	},
+	Carousel.prototype._resize=function(){
+		var _this=this;
+		var oriVal=this._findWH();
+		$(window).resize(function(event) {
+			clearInterval(_this.config._timer);
+			var newVal=_this._findWH();
+			if(oriVal != newVal){
+				_this.config.showWH= newVal;
+				_this._setMoveDis();
+				console.log(_this);
+				var curVal=Math.abs(_this._transformVal()[0] );
+				// console.log(curVal)
+				_this._findNearSteps(curVal);
+			}
+		});
+
+	},
+	Carousel.prototype._findWH=function(){
+		var direction=this._moveDirection();
+		switch(direction){
+			case 'left' : return $(this.config.element).width();break;
+			case 'top' : return $(this.config.element).height();break;
+		}
+	},
+	Carousel.prototype._findNearSteps=function(curVal){
+		var steps=0;
+		var arr=[];
+		// console.log(curVal)
+		for(var i=0 ; i <= this.config.maxMoveSteps ; i++){
+			arr[i]= { steps:i,val:Math.abs(i * this.config.steps * this.config.moveDis) - curVal};
+		}
+		arr.sort(function(n1,n2){
+			return n2.val-n1.val
+		})
+		console.log(arr);
+		this.switchTo(arr[0].steps,arr[0].steps - 1);
+
 	}
 	// Carousel.constructor=Carousel;
 	module.exports=Carousel;
